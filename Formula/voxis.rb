e2d767897c3f3af68e5d94e3525cbb6897a98052
voxis-ci <ci@voxis.top>
2026-09-19 20:29:04 +0700

    formula: update voxis v0.1.8

--- Formula/voxis.rb
+++ Formula/voxis.rb
@@ -1,9 +1,9 @@
 class Voxis < Formula
   desc "Private voice dictation engine (Tauri + Rust)"
   homepage "https://voxis.top"
-  url "https://github.com/axelbaumlisto/voxis/releases/download/v0.1.7/voxis-macos-arm64.tar.gz"
-  version "0.1.7"
-  sha256 "0bb13584a5806f104b5a55d72a99b039942386107888ce9afe193f90edfb7243"
+  url "https://github.com/axelbaumlisto/voxis/releases/download/v0.1.8/voxis-macos-arm64.tar.gz"
+  version "0.1.8"
+  sha256 "9d7f03f17b4fadcbd9d1a569e8646d3852fca12f3359cf04dd52974ec89995f9"
   depends_on arch: :arm64
 
   def install
